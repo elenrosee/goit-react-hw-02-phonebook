498544530ce838f4b2d286ca0a7944324b2c3e0e
Elena Ovcharenko <elen_rosee@MacBook-Pro-Ovcarenko.local>
2021-08-17 00:52:18 +0300

    done

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ import Notification from "./components/Notification";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
 
@@ -70,14 +65,16 @@ class App extends Component {
   };
 
   render() {
+    const { contacts, filter } = this.state;
+
     return (
       <React.Fragment>
         <h1>Phonebook</h1>
         <ContactForm onSubmit={this.formSubmitHandler} />
         <h2>Contacts</h2>
-        {this.state.contacts.length > 0 ? (
+        {contacts.length > 0 ? (
           <React.Fragment>
-            <Filter value={this.state.filter} onChange={this.changeFilter} />
+            <Filter value={filter} onChange={this.changeFilter} />
             <ContactsList
               contacts={this.getVisibleContacts()}
               onDeleteContact={this.deleteContact}
